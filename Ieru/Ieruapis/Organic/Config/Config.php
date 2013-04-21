899699c651c976351908e26e3ecfb05da4d658ef
David Baños <maketas@gmail.com>
2013-04-21 17:15:41 +0200

    Splitted content of AuthAPI and OrganicAPI, and updated the routes.

--- Ieru/Ieruapis/Organic/Config/Config.php
+++ Ieru/Ieruapis/Organic/Config/Config.php
@@ -27,9 +27,9 @@ class Config
 			$this->_routes['POST'][] = array( '/resources',     'controller'=>'OrganicAPI#fetch_resources' );
 			$this->_routes['GET'][]  = array( '/resources/:id', 'controller'=>'OrganicAPI#fetch_resource' );
 			$this->_routes['GET'][]  = array( '/search',        'controller'=>'OrganicAPI#get_search' );
-			$this->_routes['GET'][]  = array( '/login',         'controller'=>'OrganicAPI#login' );
-			$this->_routes['GET'][]  = array( '/logout',        'controller'=>'OrganicAPI#logout' );
-			$this->_routes['POST'][] = array( '/register',      'controller'=>'OrganicAPI#register' );
+			$this->_routes['GET'][]  = array( '/login',         'controller'=>'AuthAPI#login' );
+			$this->_routes['GET'][]  = array( '/logout',        'controller'=>'AuthAPI#logout' );
+			$this->_routes['POST'][] = array( '/register',      'controller'=>'AuthAPI#register' );
 		}
 		return $this->_routes;
 	}
