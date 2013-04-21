11c8137843f4fe0cbcef0a721229baa728a0e455
David Baños <maketas@gmail.com>
2013-04-21 22:04:19 +0200

    Changes for navigational search

--- Ieru/Ieruapis/Organic/OrganicAPI.php
+++ Ieru/Ieruapis/Organic/OrganicAPI.php
@@ -293,7 +293,7 @@ class OrganicAPI
                             INNER JOIN description ON identifier.FK_general=description.FK_general
                             INNER JOIN string as strings ON string.FK_general=strings.FK_general
                             INNER JOIN string as agerange ON string.FK_general=agerange.FK_typicalAgeRange 
-                            WHERE identifier.entry IN ( ?, ? ) AND string.FK_title is not NULL AND strings.FK_description is not NULL 
+                            WHERE ( identifier.entry IN ( ?, ? ) OR identifier.entry_metametadata LIKE "%'.$uri['resource'][0].'%" ) AND string.FK_title is not NULL AND strings.FK_description is not NULL 
                                   AND string.language = strings.language
                             GROUP BY string.language';
 
