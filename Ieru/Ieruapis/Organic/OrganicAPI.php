7fd08a06c6ebea267f5a2c207d1f7e7ffbfdf066
David Baños <maketas@gmail.com>
2013-04-23 02:42:17 +0200

    temporary fix for resources with multiples languages

--- Ieru/Ieruapis/Organic/OrganicAPI.php
+++ Ieru/Ieruapis/Organic/OrganicAPI.php
@@ -105,6 +105,26 @@ class OrganicAPI
      */
     public function & fetch_resource ()
     {
+        /*
+        $sql = 'select * from identifier where entry_metametadata like "oai:green-oer:%"';
+        $stmt = $this->_db->prepare( $sql );
+        $stmt->execute();
+        $fetches = $stmt->fetchAll( \PDO::FETCH_ASSOC );
+
+        foreach ( $fetches as $row )
+        {
+            $sql = 'update identifier set entry_metametadata = "'.preg_replace('/oai:green-oer:/si', '', $row['entry_metametadata']).'" where FK_general = '.$row['FK_general'];
+            $stmt = $this->_db->prepare( $sql );
+            echo $stmt->execute();
+
+        }
+        die();
+        */
+
+        // ÑAPA ALERT
+        $napa_lang = $this->_config->get_resources_langs();
+        // ÑAPA ALERT
+
         try 
         {
             # Picks up the resource in both english (always must be in english) and the user language. Will return one or two rows.
@@ -189,6 +209,13 @@ class OrganicAPI
             $results['success'] = true;
             $results['message'] = 'API resource found.';
             $results['def_lang'] = ( isset( $this->_params['lang'] ) ) ? $this->_params['lang'] : 'en';
+
+            // ÑAPA ALERT
+            if ( array_key_exists( $results['location'], $napa_lang ) )
+                $results['napa_langs'] = $napa_lang[$results['location']];
+            else
+                $results['napa_langs'] = array( $results['language'] );
+            // ÑAPA ALERT
         }
         else
         {
@@ -279,6 +306,10 @@ class OrganicAPI
     {
         $results = array();
 
+        // ÑAPA ALERT
+        $napa_lang = $this->_config->get_resources_langs();
+        // ÑAPA ALERT        
+
         # Loop the uris for getting the local LOM resource info
         if ( $uris )
         {
@@ -291,22 +322,21 @@ class OrganicAPI
                                     agerange.Text as age_range, identifier.entry as resource, string.language as info_lang,
                                     general.language as language, general.language as res_lang, string.FK_general as id, technical.location as location
                             FROM general
-                            INNER JOIN identifier ON general.PrimaryKey = identifier.FK_general
-                            INNER JOIN string ON string.FK_general = identifier.FK_general
-                            INNER JOIN technical ON identifier.FK_general=technical.FK_lom
-                            INNER JOIN description ON identifier.FK_general=description.FK_general
-                            INNER JOIN string as strings ON string.FK_general=strings.FK_general
-                            INNER JOIN string as agerange ON string.FK_general=agerange.FK_typicalAgeRange 
-                            WHERE ( identifier.entry IN ( ?, ? ) OR identifier.entry_metametadata LIKE "%'.str_replace( '/oai:scam[.]kmr[.]se:|oai:green-oer:/si', '', $uri['resource'][0] ).'%" ) AND string.FK_title is not NULL AND strings.FK_description is not NULL 
-                                  AND string.language = strings.language
+                            INNER JOIN identifier           ON general.FK_lom           = identifier.FK_general
+                            INNER JOIN string               ON string.FK_general        = identifier.FK_general
+                            INNER JOIN technical            ON identifier.FK_general    = technical.FK_lom
+                            INNER JOIN string as strings    ON string.FK_general        = strings.FK_general
+                            INNER JOIN string as agerange   ON string.FK_general        = agerange.FK_typicalAgeRange 
+                            WHERE ( identifier.entry = ? OR identifier.entry_metametadata = ? OR identifier.entry = ? OR identifier.entry_metametadata = ?) 
+                                AND string.language = strings.language
                             GROUP BY string.language';
 
                     $stmt = $this->_db->prepare( $sql );
 
                     if ( count( $uri['resource'] ) == 1 )
-                        $stmt->execute( array( trim( $uri['resource'][0] ), trim( $uri['resource'][0] ) ) );
+                        $stmt->execute( array( trim( $uri['resource'][0] ), trim( $uri['resource'][0] ), trim( $uri['resource'][0] ), trim( $uri['resource'][0] ) ) );
                     else
-                        $stmt->execute( array( trim( $uri['resource'][0] ), trim( $uri['resource'][1] ) ) );
+                        $stmt->execute( array( trim( $uri['resource'][0] ), trim( $uri['resource'][1] ), trim( $uri['resource'][1] ), trim( $uri['resource'][0] ) ) );
                     $fetches = $stmt->fetchAll( \PDO::FETCH_ASSOC );
                 }
                 # If an exception raises, return an empty array
@@ -362,6 +392,12 @@ class OrganicAPI
                     ksort( $temp['texts'] );
                     unset( $temp['title'], $temp['description'], $temp['keyword'] );
                     #$temp['entry_link'] = str_replace( '/', '@', $temp['entry'] );
+                    // ÑAPA ALERT
+                    if ( array_key_exists( $temp['location'], $napa_lang ) )
+                        $temp['napa_langs'] = $napa_lang[$temp['location']];
+                    else
+                        $temp['napa_langs'] = array( $temp['language'] );
+                    // ÑAPA ALERT
                     $results[] = $temp;
                 }
 
@@ -446,3 +482,186 @@ class OrganicAPI
         return $data;
     }
 }
+/*
+{
+  "responseHeader":{
+    "guessedLanguage":"es",
+    "translation_nl":"vissen",
+    "translation_en":"seize fish",
+    "translation_el":"Î±ÏÏ€Î¬Î¶Ï‰ ÏˆÎ±ÏÎµÏÏ‰ Î±Î»Î¹ÎµÏÏ‰",
+    "translation_lv":"konfiscÄ“t pieÄ·erties satvert",
+    "translation_pl":"chwytaÄ‡ konfiskowaÄ‡ Å‚owiÄ‡",
+    "translation_it":"afferrare prendere pescare",
+    "translation_fr":"avaler pÃªcher prendre",
+    "translation_tr":"gaspetmek kavramak tutmak",
+    "translation_hu":"elkoboz halÃ¡szik megfog",
+    "translation_et":"anastama kinni rabama konfiskeerima",
+    "translation_de":"fangen fassen",
+    "translation_sv":"gripa",
+    "translation_es":"pescar",
+    "translation_pt":"pescar agarrar confiscar",
+    "status":0,
+    "QTime":563,
+    "params":{
+      "explainOther":"",
+      "facet":"true",
+      "fl":"general_identifier,score",
+      "indent":"on",
+      "start":"0",
+      "q":"pescado",
+      "hl.fl":"",
+      "facet.field":["educationalContext",
+        "language",
+        "technicalFormat",
+        "educationalRole"],
+      "wt":"json",
+      "fq":"*:*",
+      "version":"2.2",
+      "rows":"10"}},
+  "response":{"numFound":331,"start":0,"maxScore":0.0232714,"docs":[
+      {
+        "general_identifier":["24761","http://prodinra.inra.fr/record/33159"],
+        "score":0.0232714},
+      {
+        "general_identifier":["25622","http://prodinra.inra.fr/record/91502"],
+        "score":0.015876405},
+      {
+        "general_identifier":["25384","http://prodinra.inra.fr/record/181400"],
+        "score":0.014427877},
+      {
+        "general_identifier":["11292","http://www.ruma.org.uk/"],
+        "score":0.013628369},
+      {
+        "general_identifier":["25598","http://prodinra.inra.fr/record/133600"],
+        "score":0.012403673},
+      {
+        "general_identifier":["24328","http://prodinra.inra.fr/record/161665"],
+        "score":0.012403673},
+      {
+        "general_identifier":["24299","http://prodinra.inra.fr/record/125328"],
+        "score":0.011914171},
+      {
+        "general_identifier":["24113","http://prodinra.inra.fr/record/53545"],
+        "score":0.011420921},
+      {
+        "general_identifier":["23990","http://prodinra.inra.fr/record/21484"],
+        "score":0.011420921},
+      {
+        "general_identifier":["24004","http://prodinra.inra.fr/record/128097"],
+        "score":0.010900196}]
+  },
+  "facet_counts":{
+    "facet_queries":{},
+    "facet_fields":{
+      "educationalContext":[
+        "higher education",176,
+        "vocational education",75,
+        "policy making",26,
+        "compulsory education",25,
+        "continuing education",17,
+        "Higher education",6,
+        "professional development",5,
+        "other",4,
+        "Compulsory education",3,
+        "library",3,
+        "Continuing education",1,
+        "Vocational education",1,
+        "distance education",1,
+        "Continuous Formation",0,
+        "Distance education",0,
+        "Educational administration",0,
+        "Higher Education",0,
+        "Other",0,
+        "Policy making",0,
+        "Primary Education",0,
+        "Professional Formation",0,
+        "Professional development",0,
+        "Secondary Education",0,
+        "Technical School First Cycle",0,
+        "Technical School Second Cycle",0,
+        "University First Cycle",0,
+        "University Postgraduate",0,
+        "University Second Cycle",0,
+        "Vocational Training",0,
+        "educational administration",0,
+        "pre-school",0,
+        "special education",0],
+      "language":[
+        "en",223,
+        "fr",104,
+        "or",7,
+        "de",6,
+        "es",6,
+        "no",1,
+        "ar",0,
+        "cs",0,
+        "cy",0,
+        "da",0,
+        "el",0,
+        "et",0,
+        "fi",0,
+        "hi",0,
+        "hu",0,
+        "it",0,
+        "ja",0,
+        "kn",0,
+        "lt",0,
+        "lv",0,
+        "nl",0,
+        "pl",0,
+        "pt",0,
+        "ro",0,
+        "ru",0,
+        "sk",0,
+        "sv",0,
+        "te",0,
+        "tr",0,
+        "tw",0,
+        "zh",0],
+      "technicalFormat":[
+        "application/pdf",198,
+        "text/html",50,
+        "audio/mp3",3,
+        "image/jpeg",3,
+        "XML",1,
+        "application/x-shockwave-flash",1,
+        "Application",0,
+        "Image",0,
+        "Multipart",0,
+        "Text",0,
+        "Video",0,
+        "application/msword",0,
+        "application/octet-stream",0,
+        "application/ppt",0,
+        "application/vnd.ms-excel",0,
+        "application/vnd.ms-powerpoint",0,
+        "application/vnd.ms-word",0,
+        "application/zip",0,
+        "audio/mpeg",0,
+        "image/gif",0,
+        "image/png",0,
+        "text/plain",0,
+        "video/avi",0,
+        "video/mp4",0,
+        "video/mpeg",0,
+        "video/quicktime",0,
+        "video/x-ms-wmv",0],
+      "educationalRole":[
+        "learner",257,
+        "teacher",24,
+        "manager",13,
+        "other",12,
+        "counsellor",7,
+        "author",3,
+        "parent",2,
+        "Author",0,
+        "Learner",0,
+        "Manager",0,
+        "Other",0,
+        "Teacher",0,
+        "counselor",0]},
+    "facet_dates":{},
+    "facet_ranges":{}}}
+
+
+    */
