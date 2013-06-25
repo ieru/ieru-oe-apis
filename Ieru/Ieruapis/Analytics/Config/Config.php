a0209c12272e88f7aaaa8bb38d177cb06bb51115
David Baños <maketas@gmail.com>
2013-06-26 01:51:12 +0200

    refactored a bit the Microsoft Translation service

--- Ieru/Ieruapis/Analytics/Config/Config.php
+++ Ieru/Ieruapis/Analytics/Config/Config.php
@@ -56,6 +56,7 @@ class Config
 	 */
 	public function get_default_translation_services ()
 	{
+            //return array();
 		return array( 'es'=>'xerox', 'fr'=>'xerox', 'de'=>'xerox', 'it'=>'xerox' );
 	}
 
--- Ieru/Ieruapis/Analytics/Providers/Translation/MicrosoftService.php
+++ Ieru/Ieruapis/Analytics/Providers/Translation/MicrosoftService.php
@@ -45,31 +45,15 @@ class MicrosoftService implements MultilingualTranslationAdapter
 	{
         try 
         {
-            // Client ID of the application.
-            $clientID     = 'ce294d5d-605a-4841-a8e3-b74971689e62';
-            // Client Secret key of the application.
-            $clientSecret = 'PjOAWsvyv7UFVwow98DBzK+2Y+n7Ym0czxNylk+uV3o=';
-            // OAuth Url.
-            $authUrl      = 'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/';
-            // Application Scope Url
-            $scopeUrl     = 'http://api.microsofttranslator.com';
-            // Application grant type
-            $grantType    = 'client_credentials';
-            // Get the Access token.
-            $authHeader   = 'Authorization: Bearer '.$this->getTokens( $grantType, $scopeUrl, $clientID, $clientSecret, $authUrl );
-
-            // Set the params.
+            // Set the translation parameters
             $fromLanguage = !isset( $params['from'] ) ? 'en' : $params['from'];
             $toLanguage   = $params['to'];
             $inputStr     = $params['text'];
-            $contentType  = 'text/plain';
-            $category     = 'general';
         
+            // Make the request
             $parameters   = 'text='.urlencode($inputStr).'&to='.$toLanguage.'&from='.$fromLanguage;
             $translateUrl = 'http://api.microsofttranslator.com/v2/Http.svc/Translate?'.$parameters;
-        
-            // Get the curlResponse.
-            $curlResponse = $this->curlRequest( $translateUrl, $authHeader );
+            $curlResponse = $this->_curl_request( $translateUrl, $this->_get_auth_header() );
         
             // Interprets a string of XML into an object.
             $xmlObj = simplexml_load_string( $curlResponse );
@@ -88,7 +72,86 @@ class MicrosoftService implements MultilingualTranslationAdapter
         return $translatedStr;
 	}
 
-	/**
+    /**
+     * Sends a translation request to the service
+     *
+     * @param   array     $params     Information needed to do the request
+     * @return  string   The translation
+     */
+    public function languages ( &$params )
+    {
+        try 
+        {
+            // Make the request
+            $translateUrl = 'http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForTranslate';
+            $curlResponse = $this->_curl_request( $translateUrl, $this->_get_auth_header() );
+        
+            // Interprets a string of XML into an object.
+            $xmlObj = simplexml_load_string( $curlResponse );
+            foreach ( (array)$xmlObj[0] as $val )
+                $translatedStr = $val;
+
+            // The errors of the Microsoft API are sent using HTML
+            if ( isset( $translatedStr->h1 ) )
+                throw new APIException( 'Microsoft Translate: '.$translatedStr->h1.'.', array( 'full_text'=>$curlResponse ) );
+        }
+        catch ( APIException $e )
+        {
+            $e->to_json();
+        }
+
+        return $translatedStr;
+    }
+
+    /**
+     * Create and execute the HTTP CURL request.
+     *
+     * @param string $url        HTTP Url.
+     * @param string $authHeader Authorization Header string.
+     * @param string $postData   Data to post.
+     * @return string.
+     */
+    private function _curl_request ( $url, $authHeader )
+    {
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array( $authHeader, 'Content-Type: text/xml' ) );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+        $curlResponse = curl_exec( $ch );
+        $curlErrno = curl_errno( $ch );
+        if ( $curlErrno )
+        {
+            $curlError = curl_error( $ch );
+            throw new APIException( $curlError );
+        }
+        curl_close( $ch );
+        return $curlResponse;
+    }
+
+    /**
+     * Get the authentication header for making requests to Microsoft Translate.
+     *
+     * @return  string   The authentication header
+     */
+    private function _get_auth_header ()
+    {
+        // Client ID of the application.
+        $data['clientID']     = 'ce294d5d-605a-4841-a8e3-b74971689e62';
+        // Client Secret key of the application.
+        $data['clientSecret'] = 'PjOAWsvyv7UFVwow98DBzK+2Y+n7Ym0czxNylk+uV3o=';
+        // OAuth Url.
+        $data['authUrl']      = 'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/';
+        // Application Scope Url
+        $data['scopeUrl']     = 'http://api.microsofttranslator.com';
+        // Application grant type
+        $data['grantType']    = 'client_credentials';
+
+        // Get the Access token.
+        return 'Authorization: Bearer '.$this->_get_tokens( $data['grantType'], $data['scopeUrl'], $data['clientID'], $data['clientSecret'], $data['authUrl'] );
+    }
+
+    /**
      * Get the access token.
      *
      * @param string $grantType    Grant type.
@@ -98,7 +161,7 @@ class MicrosoftService implements MultilingualTranslationAdapter
      * @param string $authUrl      Oauth Url.
      * @return string.
      */
-    function getTokens ( $grantType, $scopeUrl, $clientID, $clientSecret, $authUrl )
+    private function _get_tokens ( $grantType, $scopeUrl, $clientID, $clientSecret, $authUrl )
     {
         try 
         {
@@ -137,80 +200,5 @@ class MicrosoftService implements MultilingualTranslationAdapter
         {
             $e->to_json();
         }
-    }
-
-    /**
-     * Create and execute the HTTP CURL request.
-     *
-     * @param string $url        HTTP Url.
-     * @param string $authHeader Authorization Header string.
-     * @param string $postData   Data to post.
-     * @return string.
-     */
-    function curlRequest ( $url, $authHeader )
-    {
-        $ch = curl_init();
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array( $authHeader, 'Content-Type: text/xml' ) );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-        $curlResponse = curl_exec( $ch );
-        $curlErrno = curl_errno( $ch );
-        if ( $curlErrno )
-        {
-            $curlError = curl_error( $ch );
-            throw new APIException( $curlError );
-        }
-        curl_close( $ch );
-        return $curlResponse;
-    }
-
-    /**
-     * Sends a translation request to the service
-     *
-     * @param array     $params     Information needed to do the request
-     * @return string   The translation
-     */
-    public function languages ( &$params )
-    {
-        try 
-        {
-            // Client ID of the application.
-            $clientID     = 'ce294d5d-605a-4841-a8e3-b74971689e62';
-            // Client Secret key of the application.
-            $clientSecret = 'PjOAWsvyv7UFVwow98DBzK+2Y+n7Ym0czxNylk+uV3o=';
-            // OAuth Url.
-            $authUrl      = 'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/';
-            // Application Scope Url
-            $scopeUrl     = 'http://api.microsofttranslator.com';
-            // Application grant type
-            $grantType    = 'client_credentials';
-            // Get the Access token.
-            $authHeader   = 'Authorization: Bearer '.$this->getTokens( $grantType, $scopeUrl, $clientID, $clientSecret, $authUrl );
-
-            // Set the params.
-            $contentType  = 'text/plain';
-            $category     = 'general';
-        
-            $translateUrl = 'http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForTranslate';
-        
-            // Get the curlResponse.
-            $curlResponse = $this->curlRequest( $translateUrl, $authHeader );
-        
-            // Interprets a string of XML into an object.
-            $xmlObj = simplexml_load_string( $curlResponse );
-            foreach ( (array)$xmlObj[0] as $val )
-                $translatedStr = $val;
-
-            // The errors of the Microsoft API are sent using HTML
-            if ( isset( $translatedStr->h1 ) )
-                throw new APIException( 'Microsoft Translate: '.$translatedStr->h1.'.', array( 'full_text'=>$curlResponse ) );
-        }
-        catch ( APIException $e )
-        {
-            $e->to_json();
-        }
-
-        return $translatedStr;
     }
 }
